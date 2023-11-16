8492078251a5fb0f2c50ed77eb589c2709c279bf
Ashwin Sharma <ashwin.aksharma.p@outlook.com>
2023-11-16 15:15:19 +0530

    added more functionality to store

---
+++ Store/Products/overlayController.js
@@ -0,0 +1,12 @@
+function showImageOverlay() {
+  const myOverlay = document.getElementById("imageOverlay");
+  myOverlay.className = "visible";
+}
+
+function hideImageOverlay() {
+  const myOverlay = document.getElementById("imageOverlay");
+  myOverlay.className = "hidden";
+}
+
+showImageOverlay();
+// hideImageOverlay();
